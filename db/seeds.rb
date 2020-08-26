4ec3f405bfaa0644b0b09a9c6a7d720a7949a53a
VidensKnowledge <devydoolander@gmail.com>
2020-08-26 15:46:27 -0600

    functional model and controller for departments

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+5.times do |i|
+d = Department.create(name: "Department #{i}")
+
+# d.items.create(name: 'Item 1', price: rand(0.1..500.0))
+
+
+puts 'seeded'
+# puts d.topics
+end
